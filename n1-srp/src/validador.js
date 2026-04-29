2d0c1780b2e4b2e29249807c3090435563546dd3
Naubert <naubert.mig@gmail.com>
2026-04-29 19:50:35 -0300

    Refatorado validador de usuário "monolítico" em pequenos validadores especializados.

--- n1-srp/src/validador.js
+++ n1-srp/src/validador.js
@@ -1,6 +1,7 @@
 export function validadorUsuario(usuario) {
-  if (!usuario.email.includes('@')) return false;
-  if (usuario.password.length < 8) return false;
-  if (usuario.age < 18) return false;
-  return true;
+  return (
+      validarEmail(usuario) &&
+      validarSenha(usuario) &&
+      validarIdade(usuario)
+  );
 }
